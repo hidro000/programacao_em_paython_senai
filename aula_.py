0b28a34caa7547cd5465952069530afb1396cf28
hidro000 <pabloccasitas@gmail.com>
2025-09-18 17:54:01 -0300

    Add files via upload

---
+++ aula_.py
@@ -0,0 +1,19 @@
+numero1 = float(input(''))
+numero2 = float(input(''))
+
+print(numero1+ numero2)
+print()
+
+numero1 = float(input(''))
+numero2 = float(input(''))
+print(numero1 - numero2)
+print()
+
+numero1 = float(input(''))
+numero2 = float(input(''))
+print(numero1 * numero2)
+print()
+
+numero1 = float(input(''))
+numero2 = float(input(''))
+print(numero1 // numero2)
